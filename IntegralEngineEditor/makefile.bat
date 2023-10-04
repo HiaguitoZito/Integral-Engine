800273e979caaf1eae8ec171cab73254558c0c4c
HiaguitoZito <shadowgamers016@gmail.com>
2023-10-04 19:56:30 -0300

    commit

--- IntegralEngineEditor/makefile.bat
+++ IntegralEngineEditor/makefile.bat
@@ -1,6 +1,7 @@
-g++ ./main.cpp -g -c -I./include -o ./bin/main.o
-g++ ./bin/*.o -g -L./lib -lglfw3 -lopengl32 -lgdi32 -o ./bin/program
-cd bin
+cd ../Requirements/MinGW/bin
+g++ ../../../IntegralEngineEditor/main.cpp -g -c -I./include -o ../../../IntegralEngineEditor/bin/main.o
+g++ ../../../IntegralEngineEditor/bin/*.o -g -L./lib -lglfw3 -lopengl32 -lgdi32 -o ../../../IntegralEngineEditor/bin/program
+cd ../../../IntegralEngineEditor/bin
 program
 del program.exe
 cd ..
